294a716abd3e90c5e149700ef18780151b3a8d54
Jin M <jinm0607@gmail.com>
2020-07-10 22:52:54 -0700

    2. Exercise: Adding to the Single Page App-finished

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-function RenderDirectoryItem({campsite4, onClicka}) {
+function RenderDirectoryItem({campsite4}) {
     // console.log({campsite4, onClick})
     return (
-        <Card onClick={() => onClicka(campsite4.id)}>
+        <Card>
             <CardImg width="100%" src={campsite4.image} alt={campsite4.name} />
             <CardImgOverlay>
                 <CardTitle>{campsite4.name}</CardTitle>
@@ -19,7 +19,7 @@ console.log(props)
     const directory = props.campsitez.map(campsiter => {
         return (
             <div key={campsiter.id} className="col-md-5 m-1">
-                <RenderDirectoryItem campsite4={campsiter} onClicka={props.onClickz} />
+                <RenderDirectoryItem campsite4={campsiter} />
             </div>
         );
     });
